c96823777f9b2518fa01a7dd0cce99a8106a548d
Giheon Do <74302278+Dockerel@users.noreply.github.com>
2025-03-17 21:29:50 +0900

    feat: global하게 사용되는 기능 추가 (#8)

---
+++ src/main/java/com/gdg/Todak/common/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.gdg.Todak.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Data
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private Instant updatedAt;
+}
