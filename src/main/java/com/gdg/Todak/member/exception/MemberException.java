94b37fbb028b7bdb6b0a40d60ecf794d9d8e51ce
Giheon Do <dgh0001@naver.com>
2025-03-20 18:34:01 +0900

    feat: add swagger Authorization feature

--- src/main/java/com/gdg/Todak/member/exception/MemberException.java
+++
@@ -1,8 +0,0 @@
-package com.gdg.Todak.member.exception;
-
-public class MemberException extends RuntimeException {
-
-    public MemberException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
