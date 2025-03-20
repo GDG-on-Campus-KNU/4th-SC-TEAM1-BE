8e1b6e9dbfce729802b3067c939a24881e922ebb
Giheon Do <dgh0001@naver.com>
2025-03-20 18:36:20 +0900

    feat: add unique constraint on member username

--- src/main/java/com/gdg/Todak/member/controller/advice/MemberControllerAdvice.java
+++ src/main/java/com/gdg/Todak/member/controller/advice/MemberControllerAdvice.java
@@ -32,7 +32,7 @@ public class MemberControllerAdvice {
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(BindException.class)
+    @ExceptionHandler({BindException.class, DataIntegrityViolationException.class})
     public ApiResponse<Object> bindException(BindException e) {
         return ApiResponse.of(
                 HttpStatus.BAD_REQUEST,
