4099fbb0e5d7f6d677852700ba355108e919aaf5
gitjiho <jiho99322@gmail.com>
2025-03-23 23:48:11 +0900

    feat: 친구로직 추가

---
+++ src/main/java/com/gdg/Todak/friend/entity/Friend.java
@@ -0,0 +1,52 @@
+package com.gdg.Todak.friend.entity;
+
+import com.gdg.Todak.friend.FriendStatus;
+import com.gdg.Todak.friend.exception.BadRequestException;
+import com.gdg.Todak.member.domain.Member;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Friend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "requester_id")
+    private Member requester;
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "accepter_id")
+    private Member accepter;
+    @NotNull
+    private FriendStatus friendStatus;
+
+    public boolean checkMemberIsNotAccepter(Member member) {
+        return !this.accepter.equals(member);
+    }
+
+    public boolean checkMemberIsNotRequester(Member member) {
+        return !this.requester.equals(member);
+    }
+
+    public void acceptFriendRequest() {
+        if (!this.friendStatus.equals(FriendStatus.PENDING)) {
+            throw new BadRequestException("대기중인 친구 요청이 아닙니다.");
+        }
+        this.friendStatus = FriendStatus.ACCEPTED;
+    }
+
+    public void declinedFriendRequest() {
+        if (!this.friendStatus.equals(FriendStatus.PENDING)) {
+            throw new BadRequestException("대기중인 친구 요청이 아닙니다.");
+        }
+        this.friendStatus = FriendStatus.DECLINED;
+    }
+}
---
+++ src/main/java/com/gdg/Todak/friend/repository/FriendRepository.java
@@ -0,0 +1,25 @@
+package com.gdg.Todak.friend.repository;
+
+import com.gdg.Todak.friend.FriendStatus;
+import com.gdg.Todak.friend.entity.Friend;
+import com.gdg.Todak.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+    boolean existsByRequesterAndAccepter(Member requester, Member accepter);
+
+    List<Friend> findAllByAccepterUsernameAndFriendStatusOrRequesterUsernameAndFriendStatus
+            (String accepterName, FriendStatus friendStatus1, String requesterName, FriendStatus friendStatus2);
+
+    @Query("SELECT COUNT(f) FROM Friend f WHERE f.requester = :member AND f.friendStatus IN :statuses")
+    long countByRequesterAndStatusIn(@Param("member") Member member, @Param("statuses") List<FriendStatus> statuses);
+
+    @Query("SELECT COUNT(f) FROM Friend f WHERE f.accepter = :member AND f.friendStatus IN :statuses")
+    long countByAccepterAndStatusIn(@Param("member") Member member, @Param("statuses") List<FriendStatus> statuses);
+}
