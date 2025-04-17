a9ddafe29b17fe1f41190e62ebc0ec9aa621f26b
Giheon Do <dgh0001@naver.com>
2025-04-17 21:14:10 +0900

    refactor: auth constant 분리

---
+++ src/main/java/com/gdg/Todak/member/util/JwtConstants.java
@@ -0,0 +1,7 @@
+package com.gdg.Todak.member.util;
+
+public class JwtConstants {
+    public static final String AUTHENTICATE_USER = "authenticateUser";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+}
