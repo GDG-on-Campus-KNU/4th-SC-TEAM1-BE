d7b9b5ea2773a30b28ee886bc73544f618ece832
Giheon Do <dgh0001@naver.com>
2025-05-03 21:40:18 +0900

    hotfix: filterCode 수정(memberId -> userId)

--- src/main/java/com/gdg/Todak/member/util/AdminPageSelectBoxItems.java
+++ src/main/java/com/gdg/Todak/member/util/AdminPageSelectBoxItems.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class AdminPageSelectBoxItems {
 
     public static List<FilterCode> filterCodes = List.of(
-            new FilterCode("memberId", "멤버 ID"),
+            new FilterCode("userId", "멤버 ID"),
             new FilterCode("pointType", "포인트 타입"),
             new FilterCode("pointStatus", "포인트 상태"),
             new FilterCode("date", "날짜")
