617d64b2efbe5d0b153455f566452ea58d13f7ba
gitjiho <jiho99322@gmail.com>
2025-05-03 23:20:38 +0900

    Feat: GrowthButton과 TreeConfig 정의

---
+++ src/main/java/com/gdg/Todak/tree/domain/TreeConfig.java
@@ -0,0 +1,27 @@
+package com.gdg.Todak.tree.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum TreeConfig {
+    INITIAL_LEVEL(1),
+    INITIAL_EXPERIENCE(0),
+    LEVEL_ONE_MAX_EXPERIENCE(100),
+    LEVEL_TWO_MAX_EXPERIENCE(150),
+    LEVEL_THREE_MAX_EXPERIENCE(200),
+    LEVEL_FOUR_MAX_EXPERIENCE(250),
+    LEVEL_FIVE_MAX_EXPERIENCE(300),
+    MAX_LEVEL(5),
+    WATER_PLUS_EXPERIENCE(10),
+    SUN_PLUS_EXPERIENCE(20),
+    NUTRIENT_PLUS_EXPERIENCE(30),
+    WATER_SPEND(10),
+    SUN_SPEND(15),
+    NUTRIENT_SPEND(20);
+
+    private final int value;
+
+    TreeConfig(int value) {
+        this.value = value;
+    }
+}
