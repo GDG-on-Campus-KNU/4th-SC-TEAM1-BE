47d66e9033658ed18c3a95f12905bb6451c2d74a
gitjiho <jiho99322@gmail.com>
2025-05-14 14:12:43 +0900

    Docs: 알림 및 방명록 기능 swagger 설정

--- src/main/java/com/gdg/Todak/guestbook/controller/GuestbookController.java
+++ src/main/java/com/gdg/Todak/guestbook/controller/GuestbookController.java
@@ -8,6 +8,9 @@ import com.gdg.Todak.guestbook.controller.dto.GetGuestbookResponse;
 import com.gdg.Todak.guestbook.service.GuestbookService;
 import com.gdg.Todak.member.domain.AuthenticateUser;
 import com.gdg.Todak.member.resolver.Login;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,22 +19,26 @@ import java.util.List;
 @RequestMapping("/api/v1/guestbook")
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "방명록", description = "방명록 관련 API")
 public class GuestbookController {
 
     private final GuestbookService guestbookService;
 
     @GetMapping
-    public ApiResponse<List<GetGuestbookResponse>> getGuestbook(@Login AuthenticateUser user) {
+    @Operation(summary = "방명록 조회", description = "본인에게 작성된 방명록을 조회합니다. 작성한 방명록은 24시간동안만 유지됩니다.")
+    public ApiResponse<List<GetGuestbookResponse>> getGuestbook(@Parameter(hidden = true) @Login AuthenticateUser user) {
         return ApiResponse.ok(guestbookService.getGuestbook(user));
     }
 
     @PostMapping
-    public ApiResponse<AddGuestbookResponse> addGuestbook(@Login AuthenticateUser user, @RequestBody AddGuestbookRequest request) {
+    @Operation(summary = "방명록 작성", description = "userId에 해당하는 사람에게 방명록을 작성합니다. 작성된 방명록은 24시간동안만 유지됩니다.")
+    public ApiResponse<AddGuestbookResponse> addGuestbook(@Parameter(hidden = true) @Login AuthenticateUser user, @RequestBody AddGuestbookRequest request) {
         return ApiResponse.ok(guestbookService.addGuestbook(user, request));
     }
 
     @DeleteMapping
-    public ApiResponse<String> deleteGuestbook(@Login AuthenticateUser user, @RequestBody DeleteGuestbookRequest request) {
+    @Operation(summary = "방명록 삭제", description = "수동으로 방명록을 삭제합니다. (자동으로 24시간후에 삭제됨)")
+    public ApiResponse<String> deleteGuestbook(@Parameter(hidden = true) @Login AuthenticateUser user, @RequestBody DeleteGuestbookRequest request) {
         return ApiResponse.ok(guestbookService.deleteGuestbook(user, request));
     }
 }
